e5e47d00c7704e1bcb054b078eedb8cc86a70cc5
Artem <centr_cao@mail.ru>
2021-01-10 00:23:48 +0600

    Practice 15

---
+++ menu.inc.php
@@ -0,0 +1,9 @@
+            <nav>
+                <ul>
+                    <li><a href="#"> Обо мне </a></li>
+                    <li><a href="#"> Портфолио </a></li>
+                    <li><a href="#"> Контакты </a></li>
+                    <li><a href="#"> Ссылки </a></li>
+                    <li><a href="#"> Прочее </a></li>
+                </ul>    
+            </nav>
